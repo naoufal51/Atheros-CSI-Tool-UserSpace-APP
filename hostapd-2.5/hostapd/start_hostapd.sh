d7dd46cd8d38cb314b5e7dc0004e9456b5cea231
Naoufal <naoufal51@gmail.com>
2017-07-18 23:20:23 +0200

    from atheros to ...

--- hostapd-2.5/hostapd/start_hostapd.sh
+++ hostapd-2.5/hostapd/start_hostapd.sh
@@ -3,8 +3,8 @@
 sudo nmcli nm wifi off
 sudo rfkill unblock wlan
 
-sudo ifconfig wlan0 down
-sudo ifconfig wlan0 10.10.0.1/24 up
+sudo ifconfig atheros down
+sudo ifconfig atheros 10.10.0.1/24 up
 sleep 1
 
 sudo service isc-dhcp-server restart
